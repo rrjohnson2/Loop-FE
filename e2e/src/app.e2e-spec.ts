d23d2e8b0ac4713def1c3683b5a20ceba23e504b
Roice Johnson <rjohn230uncc.edu>
2020-04-01 17:31:41 -0400

    looop

--- e2e/src/app.e2e-spec.ts
+++ e2e/src/app.e2e-spec.ts
@@ -10,7 +10,7 @@ describe('workspace-project App', () => {
 
   it('should display welcome message', () => {
     page.navigateTo();
-    expect(page.getTitleText()).toEqual('Welcome to Loop!');
+    expect(page.getTitleText()).toEqual('Welcome to Looop!');
   });
 
   afterEach(async () => {
