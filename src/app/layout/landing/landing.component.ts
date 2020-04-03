cb65050b6ed2f26c50edf512a58fdd6e52221f93
Roice Johnson <rjohn230uncc.edu>
2020-04-03 13:53:51 -0400

    landing version one works

--- src/app/layout/landing/landing.component.ts
+++ src/app/layout/landing/landing.component.ts
@@ -1,4 +1,10 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild } from '@angular/core';
+import { Profile } from 'src/app/models/profile';
+import { Idea } from 'src/app/models/idea';
+import { Notice } from 'src/app/models/notice';
+import { LayoutService } from '../layout.service';
+import { AlertComponent } from 'src/app/shared/alerts/alert.component';
+import { ContentComponent } from '../content/content.component';
 
 @Component({
   selector: 'app-landing',
@@ -7,9 +13,31 @@ import { Component, OnInit } from '@angular/core';
 })
 export class LandingComponent implements OnInit {
 
-  constructor() { }
+  profile:Profile;
+  ideas:Idea[] =[];
+  notifications:Notice[] = [];
+
+  @ViewChild(AlertComponent) alert:AlertComponent;
+  @ViewChild(ContentComponent) content:ContentComponent;
+
+  constructor(private layout:LayoutService) { }
 
   ngOnInit() {
+    this.layout.profile.subscribe(data=> this.profile = data);
+
+    this.layout.ideas_obs.subscribe(data => this.ideas = data);
+
+    this.layout.notifications.subscribe(data => this.notifications = data);
   }
+
+  alerty(alert)
+  {
+      this.alert.add(alert);
+  }
+  showNotice(notice)
+  {
+      this.content.showNotice(notice);
+  }
+
 
 }
