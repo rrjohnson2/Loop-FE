4013af4fc6e5b6d96df6048c93cdfbdc5cb2ae50
Roice Johnson <rjohn230uncc.edu>
2020-04-30 13:32:44 -0400

    tag fixed kinda

--- src/app/layout/pipe/dateago.pipe.ts
+++ src/app/layout/pipe/dateago.pipe.ts
@@ -24,9 +24,9 @@ export class DateagoPipe implements PipeTransform {
             counter = Math.floor(seconds / intervals[i]);
             if (counter > 0)
                 if (counter === 1) {
-                    return counter +" "+ i ; // singular (1 day ago)
+                    return counter +"\u2022"+ i ; // singular (1 day ago)
                 } else {
-                    return counter +" " + i + 's'; // plural (2 days ago)
+                    return counter +"\u2022" + i + 's'; // plural (2 days ago)
                 }
         }
     }
