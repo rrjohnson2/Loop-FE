c22022f4068e7605503a653400532c53413d4304
Roice Johnson <rjohn230uncc.edu>
2020-04-30 12:15:38 -0400

    icon dynamic

--- src/app/layout/icon/icon.component.ts
+++ src/app/layout/icon/icon.component.ts
@@ -8,7 +8,7 @@ import { Component, OnInit, Input } from '@angular/core';
 export class IconComponent implements OnInit {
 
   @Input() preference:{src:string};
-
+  @Input() type;
   constructor() { }
 
   ngOnInit() {
