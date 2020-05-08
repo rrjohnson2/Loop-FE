64ab24bb93b1bce3a3ee8736374e46c64420c30e
Roice Johnson <rjohn230uncc.edu>
2020-05-07 23:39:38 -0400

    all work right now

--- src/app/layout/bit-content/bit-content.component.ts
+++ src/app/layout/bit-content/bit-content.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit, Input } from '@angular/core';
 import { here } from 'src/app/constants/app.constants';
+import { log } from 'util';
 
 @Component({
   selector: 'app-bit-content',
@@ -8,43 +9,25 @@ import { here } from 'src/app/constants/app.constants';
 })
 export class BitContentComponent implements OnInit {
 
-   bit:File;
-   private reader:FileReader;  
+   type;
    src;
 
   constructor() { }
 
   ngOnInit() {
-   this.init(null);
   }
   init(bit)
   {
-    this.bit = bit
-    this.src= null;
-    this.reader = new FileReader();
-    this.reader.onload = (e) => {
-      this.src = this.reader.result;
-  }
+    this.type = bit.type;
+    this.src= URL.createObjectURL(bit);
 
   }
 
   render_type(type:string)
   {
-    
-    this.render_src();
     if(type.match("image")) return 'image';
     else if(type.match("video")) return 'video';
     else if(type.match("audio")) return 'audio';
-
-  }
-  private render_src()
-  { 
-    here();
-    if(this.bit && this.reader.readyState ==0)
-    {
-      this.reader.readAsDataURL(this.bit);
-    }
-
 
   }
 
--- src/app/layout/pipe/safe-html.pipe.ts
+++ src/app/layout/pipe/safe-html.pipe.ts
@@ -1,6 +1,6 @@
 import { Pipe, PipeTransform } from '@angular/core';
 
-import {DomSanitizer} from '@angular/platform-browser';
+import {DomSanitizer, SafeHtml, SafeStyle, SafeScript, SafeUrl, SafeResourceUrl} from '@angular/platform-browser';
 
 @Pipe({
   name: 'safeHtml'
@@ -9,8 +9,15 @@ export class SafeHtmlPipe implements PipeTransform {
 
   constructor(private sanitizer: DomSanitizer) {
   }
-  transform(value: any, args?: any): any {
-    return this.sanitizer.bypassSecurityTrustHtml(value);
+  public transform(value: any, type: string): SafeHtml | SafeStyle | SafeScript | SafeUrl | SafeResourceUrl {
+    switch (type) {
+			case 'html': return this.sanitizer.bypassSecurityTrustHtml(value);
+			case 'style': return this.sanitizer.bypassSecurityTrustStyle(value);
+			case 'script': return this.sanitizer.bypassSecurityTrustScript(value);
+			case 'url': return this.sanitizer.bypassSecurityTrustUrl(value);
+			case 'resourceUrl': return this.sanitizer.bypassSecurityTrustResourceUrl(value);
+			default: throw new Error(`Invalid safe type specified: ${type}`);
+		}
   }
 
 }
